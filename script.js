01333fc18c6075fe1fdb5553f20cdeee5033a709
MakerFaffa <makerfaffa@gmail.com>
2023-07-25 00:35:30 +0200

    Added Current Input Field, ecc.

--- script.js
+++ script.js
@@ -110,6 +110,7 @@ var repeats = document.querySelectorAll("#activity-repeats")
 
 var actName = document.querySelectorAll("#activity-name")
 var actGoal = document.querySelectorAll("#activity-goal")
+var actCurrent = document.querySelector("#activity-current")
 var actDate = document.querySelectorAll("#activity-date")
 var actPeriod = document.querySelectorAll("#activity-repeat-period")
 
@@ -133,8 +134,6 @@ function updateRepeatActivity()
 }
 
 updateRepeatActivity()
-
-
 
 
 
@@ -182,6 +181,7 @@ const addActivity = async(event) =>
         actName[0].value = "";
         actGoal[0].value = "";
         actDate[0].value = "";
+        actCurrent.value = "";
         repeats[0].checked = false;
         updateRepeatActivity()
     }
@@ -213,14 +213,17 @@ const loadActivities = async() =>
     
         return `
         <div class="activity" id=${item.id} onclick="openEditActivity(${item.id})">
-            <h1 class="name">${item.activity} (${parseInt(current / item.goal * 100)}%)</h1>
+            <div class="title">
+                <h1>${item.activity}</h1>
+                <p>${parseInt(current / item.goal * 100)}%</p>
+            </div>
             <div class="info">
                 <div class="text">
                     <p class="current">${current}</p>
                     <p class="goal">${item.goal}</p>
                 </div>
             <div class="progress-bar">
-                <div class="progress" style="width: calc(${parseInt(current / item.goal * 100)}% - 6px);"></div>
+                <div class="progress" style="width: calc(${parseInt(current / item.goal * 100)}% - 3%);"></div>
             </div>
         </div>
         <p class="date">${dateText}</p>
@@ -290,7 +293,6 @@ window.onload = loadActivities
 
 
     
-const currentLabel = editActivityPage.querySelector(".current .info .text .current")
 const goalLabel = editActivityPage.querySelector(".main .info .text .goal")
 var currentActivityId = -1;
 
@@ -317,7 +319,7 @@ function editActivity(id) {
         actGoal[1].value = goal;
         repeats[1].checked = repeat;
         
-        currentLabel.innerHTML = current;
+        actCurrent.value = current;
         goalLabel.innerHTML = goal;
 
         if(repeat) actPeriod[1].value = date;
@@ -327,9 +329,9 @@ function editActivity(id) {
     })
 }
 
-
-const saveActivity = async () => {
+const saveActivity = async (closePage) => {
     var activity = actName[1].value;
+    var current = actCurrent.value;
     var goal = actGoal[1].value;
     var repeat = repeats[1].checked;
 
@@ -347,12 +349,18 @@ const saveActivity = async () => {
     if (month < 10) month = "0" + month
 
     let currentDate = `${year}-${month}-${day}`;
-
-    await db.items.update(currentActivityId, { activity: activity, goal: goal, repeats: repeat, date: date})
-    await loadActivities();
-
-    editActivityPage.style.display = "none";
-    changePage(1);
+     
+    if (parseInt(goal) < parseInt(current)) current = goal
+    
+    db.items.update(currentActivityId, { activity: activity, goal: goal, current: current, repeats: repeat, date: date})
+    await editActivity(currentActivityId);
+    await loadActivities()
+    
+    if (closePage)
+    {
+        editActivityPage.style.display = "none";
+        await changePage(1);
+    }
 }
 
 const removeActivity = async () => {
@@ -368,14 +376,11 @@ const editProgressBar = editActivityPage.querySelector(".progress-bar .progress"
 function updateProgressBar(goal, current)
 {
     editProgressBar.style.width = "calc(" + current / goal * 100 + "% - 6px)";
-
-    currentLabel.innerHTML = current;
 }
+
 
 const addCurrent = async() =>
 {
-    var newCurrent;
-
     db.open().then(function () {
         return db.items
             .where('id')
@@ -384,32 +389,34 @@ const addCurrent = async() =>
     }).then(function (items) { 
         if(items[0].current < items[0].goal) 
         {
-            newCurrent = items[0].current + 1 
+            actCurrent.value = items[0].current + 1 
         }
-        else newCurrent = items[0].current;
+        else actCurrent.value = items[0].current;
 
-        db.items.update(currentActivityId, { current: newCurrent })
-        updateProgressBar(items[0].goal, newCurrent)
+        db.items.update(currentActivityId, { current: actCurrent.value })
+        editActivity(currentActivityId)
+        saveActivity(false)
     })
-
+    
 }
 
 const removeCurrent = async() =>
 {
     var newCurrent;
-
+    
     db.open().then(function () {
         return db.items
-            .where('id')
-            .equals(currentActivityId)
-            .toArray();
+        .where('id')
+        .equals(currentActivityId)
+        .toArray();
     }).then(function (items) {
         if (items[0].current > 0) {
-            newCurrent = items[0].current - 1
+            actCurrent.value = items[0].current - 1
         }
-        else newCurrent = items[0].current;
-
-        db.items.update(currentActivityId, { current: newCurrent })
-        updateProgressBar(items[0].goal, newCurrent)
+        else actCurrent.value = items[0].current;
+        
+        db.items.update(currentActivityId, { current: actCurrent.value })
+        editActivity(currentActivityId)
+        saveActivity(false)
     })
 }
